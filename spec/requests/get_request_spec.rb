83a5bd5e4d140874bd0344b85fc3da06e010473f
Sid Ngeth <sngeth@gmail.com>
2016-08-10 23:17:05 -0400

    Write test to get latest searches

--- spec/requests/get_request_spec.rb
+++ spec/requests/get_request_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "GET imagesearch index" do
-  let(:expected_json) do
-    [{
-     "url": "http://i.imgur.com/TrHZQ4n.jpg"
-    }].to_json
+RSpec.describe "GET requests" do
+  describe "GET api/imagesearch" do
+    let(:expected_json) do
+      [{
+        "url": "http://i.imgur.com/TrHZQ4n.jpg"
+      }].to_json
+    end
+
+    it "can get the image URL's when given a search string" do
+      json = File.read(File.join("spec", "json", "imgur_gallery.json"))
+      stub_request(:get, "https://api.imgur.com/3/" +
+                   "gallery/search?q=lolcats%20funny").
+                   to_return(:status => 200,
+                             :body => json,
+                             :headers => {})
+
+      get '/api/imagesearch/lolcats%20funny?offset=10'
+
+      expect(WebMock).to have_requested(:get, "https://api.imgur.com/3/gallery/search")
+        .with(query: {"q" => "lolcats funny"})
+
+      expect(response.body).to eq expected_json
+    end
   end
 
-  it "can get the image URL's when given a search string" do
-
-    json = File.read(File.join("spec", "json", "imgur_gallery.json"))
-    stub_request(:get, "https://api.imgur.com/3/" +
-                 "gallery/search?q=lolcats%20funny").
-                 to_return(:status => 200,
-                           :body => json,
-                           :headers => {})
-
-    get '/api/imagesearch/lolcats%20funny?offset=10'
-
-    expect(WebMock).to have_requested(:get, "https://api.imgur.com/3/gallery/search")
-      .with(query: {"q" => "lolcats funny"})
-
-    expect(response.body).to eq expected_json
+  describe "GET latest" do
+    pending "can show the latest search results" do
+      get '/latest/imagesearch'
+      latest_results = Searches.first.as_json
+      expect(response.body).to eq latest_results
+    end
   end
 end
